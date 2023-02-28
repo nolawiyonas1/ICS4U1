3ccb96d4699a06373a3963d7a722e91d16bf8c87
Nolawi Teklehaimanot <nolawiteklehaimanot@gmail.com>
2023-02-28 11:44:19 -0500

    Update Static Methods.py

--- Unit 9/Static Methods.py
+++ Unit 9/Static Methods.py
@@ -1,18 +1,21 @@
 class Fraction:
-      
-  def __init__ (self, n, d):
-    if d < 0:
-      self.__num = -n
-      self.__den = -d
-    else:
-      self.__num = n
-      self.__den = d
-      
-  def times(self, other):
-    result = Fraction(self.__num*other.__num,self.__den*other.__den)
-    return result
+    def __init__(self, n, d): 
+        self.num = n
+        self.den = d
     
-  @staticmethod # function decorator
-  def product(f1,f2):
-    result = Fraction(f1.__num*f2.__num,f1.__den*f2.__den)
-    return result
+    @staticmethod # function decorator
+    def product(f1,f2):
+        final_product = Fraction(f1.__num*f2.__num,f1.__den*f2.__den)
+        return final_product
+
+    @staticmethod # function decorator
+    def absolute(f1):
+        final_absolute = Fraction(abs(f1.num), abs(f1.den))
+        return final_absolute
+    
+    @staticmethod # function decorator
+    def is_positive(f1):
+        if f1.num > 0:
+            return True
+        else:
+            return False
