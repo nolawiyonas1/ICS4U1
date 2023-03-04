474f85252ff37feeb607e55bf4ddebb99fc7155e
Nolawi Teklehaimanot <nolawiteklehaimanot@gmail.com>
2023-03-03 21:40:22 -0500

    Update Static Methods.py

--- Unit 9/Static Methods.py
+++ Unit 9/Static Methods.py
@@ -1,5 +1,3 @@
-#### INCOMPLETE ####
-
 import math
 
 class Fraction:
@@ -53,23 +51,49 @@ def user_input_1_fraction():
         except:
             print("Please enter an integer")
 
-def start():
-    inputs = user_input_2_fractions()
-    fractions_product = Fraction.product(inputs[0], inputs[1], inputs[2], inputs[3])  
-    print(f"The product of the two fractions is {fractions_product}")
+def choices():
+    print()
+    print("A: Return the product of two fractions")
+    print("B: Return the absolute value of a fraction")
+    print("C: Check if a fraction is positive")
+    print()
+    choice = input("Enter your choice (A, B, C): ")
 
-    inputs_2 = user_input_1_fraction()
-    fraction_absolute = Fraction.absolute(inputs_2[0], inputs_2[1])
-    print(f"The absolute value of the fraction is {fraction_absolute}")
+    if choice == "A" or choice == "a":
+        inputs = user_input_2_fractions()
+        fractions_product = Fraction.product(inputs[0], inputs[1], inputs[2], inputs[3])  
+        print(f"The product of the two fractions is {fractions_product}")
+        print()
 
-    inputs_3 = user_input_1_fraction()
-    fraction_is_positive = Fraction.is_positive(inputs_3[0])
-    if fraction_is_positive == True:
-      print("True: The fraction is positive")
+    if choice == "B" or choice == "b":
+        inputs_2 = user_input_1_fraction()
+        fraction_absolute = Fraction.absolute(inputs_2[0], inputs_2[1])
+        print(f"The absolute value of the fraction is {fraction_absolute}")
+        print()
 
-    elif fraction_is_positive == False:
-      print("False: The fraction is negative")
+    if choice == "C" or choice == "c":
+        inputs_3 = user_input_1_fraction()
+        fraction_is_positive = Fraction.is_positive(inputs_3[0])
+        if fraction_is_positive == True:
+            print("True: The fraction is positive")
+
+        elif fraction_is_positive == False:
+            print("False: The fraction is negative")
+        print()
     
+def start(): # this  funciton will loop until the user indicates that they are done using the program
+  choice = input("Do you want to start? (y,n) ") # asking the user to start
+  while choice != 'y' and choice != 'Y' and choice != "n" and choice != "N": # ensuring the user enters a proper input
+    choice = input("Please enter 'y' or 'n': ") 
+
+  while choice == 'y' or choice == 'Y': 
+    choices() # calling the choices function if the user wants to start
+    choice = input('Do you want to run the program again? (y,n): ') # asking the user if they want to start again
+    while choice != 'y' and choice != 'Y' and choice != "n" and choice != "N": # ensuring the user enters a proper input
+      choice = input("Please enter 'y' or 'n': ") 
+
+  if choice == 'n' or choice == "N":
+    print("Thank you for using my program.") # print this if the user does not want to run the program 
 
 if __name__ == '__main__': 
     start() # calling the start function
